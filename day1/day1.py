5d35c29b2e6fd18b1a39e3f4aa96cbdb29228fdd
Frank van Mourik <frank.vanmourik@poldervalley.com>
2022-12-01 10:51:21 +0100

    day 1

---
+++ day1/day1.py
@@ -0,0 +1,37 @@
+def read_file():
+    data = []
+    for x in open("input1.txt"):
+        data.append(x[:-1])
+    return data
+
+
+def result1():
+    data = read_file()
+    sums = []
+    som = 0
+    for x in data:
+        if x:
+            som += int(x)
+        else:
+            sums.append(som)
+            som = 0
+    print(max(sums))
+
+
+def result2():
+    data = read_file()
+    sums = []
+    som = 0
+    for x in data:
+        if x:
+            som += int(x)
+        else:
+            sums.append(som)
+            som = 0
+    sums.sort(reverse=True)
+    print(sums[0]+sums[1]+sums[2])
+
+
+if __name__ == "__main__":
+    result1()
+    result2()
