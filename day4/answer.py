c7b1204d08569045de9665d26b99c5850fd9b311
Frank van Mourik <frank.vanmourik@poldervalley.com>
2022-12-04 21:15:55 +0100

    Day 4

--- day4/answer.py
+++ day4/answer.py
@@ -7,12 +7,30 @@ def read_file():
 
 def answer1():
     data = read_file()
-    print()
+    overlap = 0
+    for line in data:
+        line = line.split(',')
+        first = line[0].split('-')
+        second = line[1].split('-')
+        first = [int(x) for x in first]
+        second = [int(x) for x in second]
+        if first[0] >= second[0] and first[1] <= second[1] or first[0] <= second[0] and first[1] >= second[1]:
+            overlap += 1
+    print(overlap)
 
 
 def answer2():
     data = read_file()
-    print()
+    overlap = 0
+    for line in data:
+        line = line.split(',')
+        first = line[0].split('-')
+        second = line[1].split('-')
+        first = [int(x) for x in first]
+        second = [int(x) for x in second]
+        if first[0] <= second[0] <= first[1] or second[0] <= first[0] <= second[1]:
+            overlap += 1
+    print(overlap)
 
 
 if __name__ == "__main__":
