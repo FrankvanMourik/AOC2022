92cb8a08af20cbc7fb200ea3b3a79d045a0f7f71
Frank van Mourik <frank.vanmourik@poldervalley.com>
2022-12-04 21:06:25 +0100

    Day 3

--- template/answer.py
+++ template/answer.py
@@ -5,14 +5,47 @@ def read_file():
     return data
 
 
+def strIntersection(s1, s2):
+  out = ""
+  for c in s1:
+    if c in s2 and not c in out:
+      out += c
+  return out
+
+
 def answer1():
     data = read_file()
-    print()
+    overlap = []
+    for x in data:
+        half = len(x)//2
+        x1 = x[:half]
+        x2 = x[half:]
+        overlap.append(strIntersection(x1, x2))
+    som = 0
+    for x in overlap:
+        if x < 'a':
+            som += ord(x)-38
+        else:
+            som += ord(x)-96
+    print(som)
 
 
 def answer2():
     data = read_file()
-    print()
+    badges = []
+    for x in range(len(data)//3):
+        first = data[3*x]
+        second = data[3*x+1]
+        third = data[3*x+2]
+        temp = strIntersection(first, second)
+        badges.append(strIntersection(temp, third))
+    som = 0
+    for x in badges:
+        if x < 'a':
+            som += ord(x) - 38
+        else:
+            som += ord(x) - 96
+    print(som)
 
 
 if __name__ == "__main__":
